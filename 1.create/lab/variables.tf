f4bcc3bdd94395c1531f538e4974bd9cd4fe4aad
sh33pdog <keiron.shepherd@gmail.com>
2023-09-19 09:06:25 +0100

    Update variables.tf

--- 1.create/lab/variables.tf
+++ 1.create/lab/variables.tf
@@ -1,3 +1,14 @@
-variable bigip {}
-variable username {}
-variable password {}
+variable "bigip" {
+            type = string
+            default = "10.1.1.9"
+        }
+        
+        variable "username" {
+          type = string
+          default = "admin"
+        }
+        
+        variable "password" {
+            type = string
+            default = "xxxx"
+        }
